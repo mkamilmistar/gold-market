b2f46ec219e37c5980e3b2bd88dd5aa0a58cebc1
M. Kamil <mk.kamiil@gmail.com>
2021-06-11 10:16:36 +0700

    producct component at home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ComponentModule } from './component/component.module';
+import { ComponentModule } from './shared/components/component.module';
 import { HomeModule } from './pages/home/home.module';
 
 @NgModule({
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
-import { ComponentModule } from 'src/app/component/component.module';
-
-
+import { ComponentModule } from 'src/app/shared/components/component.module';
+import { JumbotronComponent } from './components/jumbotron/jumbotron.component';
+import { ProductComponent } from './components/product/product.component';
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    JumbotronComponent,
+    ProductComponent
   ],
   imports: [
     CommonModule,
