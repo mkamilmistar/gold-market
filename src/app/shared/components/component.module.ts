044b5b0cab1a60f5c34b96803ccecfa97815b608
M. Kamil <mk.kamiil@gmail.com>
2021-06-11 10:30:38 +0700

    Footer initialize

--- src/app/shared/components/component.module.ts
+++ src/app/shared/components/component.module.ts
@@ -2,19 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CarouselComponent } from './carousel/carousel.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
     NavbarComponent,
-    CarouselComponent
+    CarouselComponent,
+    FooterComponent,
   ],
   imports: [
     CommonModule,
   ],
   exports: [
     NavbarComponent,
-    CarouselComponent
+    CarouselComponent,
+    FooterComponent
   ]
 })
 export class ComponentModule { }
